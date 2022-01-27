fb65eae152cc44c8410c0fcac5df6029af5ff104
luizfernandoliveira <luiz.fernando_1990@hotmail.com>
2022-01-26 23:06:21 -0300

    Iniciado o cadastro de erros da página empresa.

--- empresa/urls.py
+++ empresa/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastro/', views.cadastro_empresa, name='cadastro'),
+    path('cadastro/', views.cadastro_empresa, name='cadastro_empresa'),
     path('lista/', views.lista_empresa, name='lista'),
 ]
