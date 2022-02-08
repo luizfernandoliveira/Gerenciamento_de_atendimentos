f11b8995a7f29cae643cb6f314e4a8d40171d89b
luizfernandoliveira <luiz.fernando_1990@hotmail.com>
2022-02-08 01:04:29 -0300

    removido o botão contrato ativo e configurado os campos obrigatórios do cadastro de empresa

--- empresa/views.py
+++ empresa/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Empresas
+from datetime import date
 
 
 def cadastro_empresa(request):
@@ -10,19 +11,35 @@ def cadastro_empresa(request):
 def valida_cadastro_empresa(request):
     nome = request.POST.get('nome')
     tipo_contrato = request.POST.get('tipo_contrato')
-    # data_de_cadastro = request.POST.get('data_de_cadastro')
-    # data_final_contrato = request.POST.get('data_final_contrato')
+    data_de_cadastro = request.POST.get('data_de_cadastro')
+    data_final_contrato = request.POST.get('data_final_contrato')
     responsavel_manutencao = request.POST.get('responsavel_manutencao')
     telefone_responsavel = request.POST.get('telefone_responsavel')
 
+
     empresa = Empresas.objects.filter(nome=nome)
 
+    if len(nome.strip()) == 0:
+        return redirect('/empresa/cadastro/?status=2')
+
     try:
-        empresa = Empresas(nome=nome,
-                           tipo_contrato=tipo_contrato,
-                           responsavel_manutencao=responsavel_manutencao,
-                           telefone_responsavel=telefone_responsavel
-                           )
+        if data_de_cadastro == '':
+            data_de_cadastro = date.today()
+        if data_final_contrato == '':
+            empresa = Empresas(nome=nome,
+                               tipo_contrato=tipo_contrato,
+                               responsavel_manutencao=responsavel_manutencao,
+                               telefone_responsavel=telefone_responsavel,
+                               data_de_cadastro=data_de_cadastro,
+                               )
+        else:
+            empresa = Empresas(nome=nome,
+                               tipo_contrato=tipo_contrato,
+                               responsavel_manutencao=responsavel_manutencao,
+                               telefone_responsavel=telefone_responsavel,
+                               data_de_cadastro=data_de_cadastro,
+                               data_final_contrato=data_final_contrato
+                               )
         empresa.save()
 
         return redirect('/empresa/cadastro/?status=0')
