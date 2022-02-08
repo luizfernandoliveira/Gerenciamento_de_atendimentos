e4870508baaa20b333aad3238ff7ce7c3c33129d
luizfernandoliveira <luiz.fernando_1990@hotmail.com>
2022-02-08 01:44:08 -0300

    Adicionado somente leitura para os campos de cadastro  de empresas

--- empresa/admin.py
+++ empresa/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Empresas
 
-admin.site.register(Empresas)
+# admin.site.register(Empresas)
+# readonly_fields = ('nome', 'tipo contrato', 'data de cadastro',
+#                        'data final contrato', 'responsavel manutencao', 'telefone responsavel')
+
+
+@admin.register(Empresas)
+class UsuarioAdmin(admin.ModelAdmin):
+    readonly_fields = ('nome', 'tipo_contrato', 'data_de_cadastro', 'data_final_contrato',
+                       'responsavel_manutencao', 'telefone_responsavel')
